06a889d1e1400b22138538a407d587a12f6935da
CZ <cz@ntnl.io>
2026-07-26 09:51:55 +0800

    🐛 保留未保存 View 的预览状态 (#17)

--- src/view-route.ts
+++ src/view-route.ts
@@ -1,3 +1,6 @@
 export const viewIdFromHash = (hash: string) => hash.replace(/^#/, '');
 
 export const viewHash = (viewId: string) => `#${encodeURIComponent(viewId)}`;
+
+export const shouldRestoreStoredView = (hasEditingSession: boolean, currentViewIsStored: boolean) =>
+  !hasEditingSession && !currentViewIsStored;
